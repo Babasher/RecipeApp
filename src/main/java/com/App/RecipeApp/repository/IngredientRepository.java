84c09c931fecdc484db69ffd6ee25b05d1653e93
Omar <omohammed7869@gmail.com>
2021-11-05 17:07:40 -0500

    Added custom JPQL Querie to search for ingredient by name

--- src/main/java/com/App/RecipeApp/repository/IngredientRepository.java
+++ src/main/java/com/App/RecipeApp/repository/IngredientRepository.java
@@ -1,9 +1,18 @@
 package com.App.RecipeApp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.App.RecipeApp.entity.Ingredient;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer>{
-
+	@Query("FROM Ingredient WHERE name = ?1")
+	Optional<Ingredient> findByName(String name);
+	
+	/*
+	@Query(value = "Select name From Ingredient where name = :name")
+	Ingredient findByName(String name);
+	*/
 }
