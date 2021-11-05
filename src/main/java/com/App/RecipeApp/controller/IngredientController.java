d698d1abf639b0fc5585d01bd65e821a86f0db8c
Omar <omohammed7869@gmail.com>
2021-11-05 17:10:45 -0500

    Mapped the ingredientByName in controller class

--- src/main/java/com/App/RecipeApp/controller/IngredientController.java
+++ src/main/java/com/App/RecipeApp/controller/IngredientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,11 @@ public class IngredientController {
 	@GetMapping
 	public List<Ingredient> getAllIngredients() {
 		return ingredientService.getAllIngredients();
+	}
+	
+	//build create REST API
+	@GetMapping("{name}")
+	public ResponseEntity<Ingredient> getIngredientByName(@PathVariable("name") String name) {
+		return new ResponseEntity<Ingredient>(ingredientService.getIngredientByName(name), HttpStatus.OK);
 	}
 }
